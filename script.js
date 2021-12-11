73edf3c681d5fdd5b58c221b64489b31c6381273
cbochet <95982489+cbochet@users.noreply.github.com>
2021-12-11 16:13:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// alert('bonjour')
+let counterContainer = document.getElementById("website-counter");
+let visitCount = localStorage.getItem("page_view");
+
+visitCount = Number(visitCount) + 1;
+
+localStorage.setItem("page_view", visitCount);
+
+document.getElementById("website-counter").innerText = "Visiteurs: " + visitCount;
